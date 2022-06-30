2420fabe8094a0f31a5cfb41b312f17d4b0fa668
robercijejus <robercijejus@gmail.com>
2022-06-30 13:39:00 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,11 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package boardgame;
 
 public class Board {
-
+    
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+    
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+    
+    public int getRows() {
+        return rows;
+    }
+    
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+    
+    public int getColumns() {
+        return columns;
+    }
+    
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+    
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+    
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,11 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package boardgame;
 
 public class Piece {
-
+    
+    protected Position position;
+    private Board board;
+    
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+    
+    protected Board getBoard() {
+        return board;
+    }
 }
